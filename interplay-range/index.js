189e9ddbea4a7f507ca5f450662954c08798bfd4
Hugh Kennedy <hughskennedy@gmail.com>
2015-08-09 22:30:12 -0700

    interplay-range ready to go

--- interplay-range/index.js
+++ interplay-range/index.js
@@ -3,6 +3,7 @@ const clamp = require('clamp')
 const domify = require('domify')
 const css = require('defaultcss')
 const path = require('path')
+const vkey = require('vkey')
 const fs = require('fs')
 
 const html = fs.readFileSync(
@@ -22,6 +23,7 @@ function InterplayRange (node, options) {
   const el = domify(html)
   const innerLabel = el.querySelector('.interplay-range-slider-inner > label')
   const outerLabel = el.querySelector('.interplay-range-slider > label')
+  const valueLabel = el.querySelector('.interplay-range-slider span')
   const innerSlide = el.querySelector('.interplay-range-slider-inner')
   const outerSlide = el.querySelector('.interplay-range-slider')
   const nudge = [
@@ -29,6 +31,7 @@ function InterplayRange (node, options) {
     el.querySelector('.interplay-range-nudge-right')
   ]
 
+  node.value = 'value' in options ? options.value : 0
   innerLabel.innerHTML =
   outerLabel.innerHTML = escape(options.label)
 
@@ -39,21 +42,43 @@ function InterplayRange (node, options) {
   nudgeLabels[0].innerHTML = options.keys.down || '&#9664;'
   nudgeLabels[1].innerHTML = options.keys.up || '&#9654;'
 
+  if (!node.enabled) {
+    el.classList.add('interplay-range-disabled')
+  }
+
+  node.on('enable', function () {
+    el.classList.remove('interplay-range-disabled')
+  }).on('disable', function () {
+    el.classList.add('interplay-range-disabled')
+  })
+
   node.on('change', function (next, prev) {
-    const progress = (next - options.min) / (options.max - options.min)
-    innerSlide.style.width = (progress * 100) + '%'
+    updateSlider(next)
   }).on('init', function () {
     outerSlide.addEventListener('mousedown', mousedown, false)
+    nudge[0].addEventListener('mousedown', nudgedown[0], false)
+    nudge[1].addEventListener('mousedown', nudgedown[1], false)
+    window.addEventListener('mouseup', nudgeup[0], false)
+    window.addEventListener('mouseup', nudgeup[1], false)
     window.addEventListener('mousemove', mousemove, false)
     window.addEventListener('mouseup', mouseup, false)
+    window.addEventListener('keydown', keydown, false)
+    window.addEventListener('keyup', keyup, false)
   }).on('stop', function () {
     outerSlide.removeEventListener('mousedown', mousedown, false)
+    nudge[0].removeEventListener('mousedown', nudgedown[0], false)
+    nudge[1].removeEventListener('mousedown', nudgedown[1], false)
+    window.removeEventListener('mouseup', nudgeup[0], false)
+    window.removeEventListener('mouseup', nudgeup[1], false)
     window.removeEventListener('mousemove', mousemove, false)
     window.removeEventListener('mouseup', mouseup, false)
+    window.removeEventListener('keydown', keydown, false)
+    window.removeEventListener('keyup', keyup, false)
   })
 
   var dragging = false
   function mousedown (e) {
+    if (!node.enabled) return
     dragging = true
     el.classList.add('interplay-range-dragging')
   }
@@ -63,13 +88,63 @@ function InterplayRange (node, options) {
     el.classList.remove('interplay-range-dragging')
   }
 
+  function keydown (e) {
+    const key = vkey[e.keyCode]
+    if (key === options.keys.down) {
+      nudgedown[0]()
+    }
+    if (key === options.keys.up) {
+      nudgedown[1]()
+    }
+  }
+
+  function keyup (e) {
+    const key = vkey[e.keyCode]
+    if (key === options.keys.down) {
+      nudgeup[0]()
+    }
+    if (key === options.keys.up) {
+      nudgeup[1]()
+    }
+  }
+
   function mousemove (e) {
     if (!dragging) return
 
     const bounds = outerSlide.getBoundingClientRect()
-    const progress = clamp((e.clientX - bounds.left) / bounds.width, 0, 1)
+    var value = (e.clientX - bounds.left) / bounds.width
 
-    node.value = options.min + (options.max - options.min) * progress
+    value = options.min + (options.max - options.min) * value
+    value = clamp(value, options.min, options.max)
+    node.value = options.step
+      ? Math.round(value / options.step) * options.step
+      : value
+  }
+
+  const nudgedown = [-1, +1].map(function (direction, i) {
+    return function (e) {
+      if (!node.enabled) return
+      var value = node.value
+
+      value += direction * (options.nudge || 0.1)
+      value = clamp(value, options.min, options.max)
+      node.value = value
+
+      nudge[i].classList.add('interplay-range-nudge-active')
+    }
+  })
+
+  const nudgeup = [-1, +1].map(function (direction, i) {
+    return function (e) {
+      nudge[i].classList.remove('interplay-range-nudge-active')
+    }
+  })
+
+  updateSlider(node.value)
+  function updateSlider (value) {
+    const progress = (value - options.min) / (options.max - options.min)
+    innerSlide.style.width = (progress * 100) + '%'
+    valueLabel.innerHTML = value
   }
 
   return el
--- interplay/demo.js
+++ interplay/demo.js
@@ -7,13 +7,12 @@ const gui = IP().attach()
 gui.add('bang1', Bang, { label: 'Test Bang #1', keys: { toggle: 'K', button: 'J' } })
 gui.add('bang2', Bang, { label: 'Test Bang #2', keys: { toggle: 'H', button: 'G' } })
 gui.add('bang3', Bang, { label: 'Test Bang #3' })
-gui.add('bang4', Bang, { label: 'Test Bang #4' })
-gui.add('bang5', Bang, { label: 'Test Bang #5' })
 
-gui.add('range6', Range, {
+gui.add('range1', Range, {
   label: 'Test Range #1',
   value: 0,
   nudge: 1,
+  step: 1,
   min: -10,
   max: +10,
   keys: {
@@ -21,3 +20,6 @@ gui.add('range6', Range, {
     down: 'A'
   }
 })
+
+gui.add('bang4', Bang, { label: 'Test Bang #4' })
+gui.add('bang5', Bang, { label: 'Test Bang #5' })
